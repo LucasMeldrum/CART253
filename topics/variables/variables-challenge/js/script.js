0f5be3fc94bed6c28ac95ddf2bc02b1afaabc302
LucasMeldrum <150073579+LucasMeldrum@users.noreply.github.com>
2025-09-18 15:16:38 -0400

    finished mrFurious challenge

--- topics/variables/variables-challenge/js/script.js
+++ topics/variables/variables-challenge/js/script.js
@@ -13,6 +13,8 @@ let mrFurious = {
     x: 200,
     y: 200,
     size: 100,
+    // Rage
+    rage: 1,
     // Colour
     fill: {
         r: 255,
@@ -28,11 +30,41 @@ let sky = {
     y: 400,
     // Color 
     fill: {
-        r: 255,
-        g: 225,
-        b: 225
+        r: 160,
+        g: 180,
+        b: 200
     }
-}
+};
+
+// Flying bird
+let bird = {
+    // Position
+    body: {
+        x1: 30,
+        y1: 75,
+        x2: 58,
+        y2: 35,
+        x3: 150,    
+        y3: 75,
+    },
+    // Eye of the bird
+    eye: {
+        x: 65,
+        y: 55,
+        size: 15
+    },
+    // Color
+    fill: {
+        r: 255,
+        g: 255,
+        b: 255
+    },
+    // Speed
+    velocity: {
+        x: 1,
+        y: 0
+    }
+};
 
 /**
  * Create the canvas
@@ -45,10 +77,16 @@ function setup() {
  * Draw (and update) Mr. Furious
  */
 function draw() {
-  background(160, 180, 200);
+  background(sky.fill.r, sky.fill.g, sky.fill.b);
   
+  // Make mrFurious redder
   mrFurious.fill.g -= 0.5;
   mrFurious.fill.b -= 0.5;
+
+  //Make the sky black
+  sky.fill.r -= 0.75;
+  sky.fill.g -= 0.75;
+  sky.fill.b -= 0.5;
 
   // Draw Mr. Furious as a coloured circle
   push();
@@ -56,4 +94,39 @@ function draw() {
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
   ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
   pop();
+
+  // Make mrFurious get madder and madder (shaking) and constrain it
+  mrFurious.rage += 0.025
+
+  mrFurious.x += random(mrFurious.rage);
+  mrFurious.y += random(mrFurious.rage);
+  mrFurious.x -= random(mrFurious.rage);
+  mrFurious.y -= random(mrFurious.rage);
+
+  mrFurious.x = constrain(mrFurious.x, 0, width);
+  mrFurious.y = constrain(mrFurious.y, 0, height);
+  mrFurious.rage = constrain(mrFurious.rage, 0, 25);
+
+  // Draw the bird 
+  // Body
+  push();
+  noStroke();
+  fill(bird.fill.r, bird.fill.g, bird.fill.b);
+  triangle(bird.body.x1, bird.body.y1, 
+           bird.body.x2, bird.body.y2, 
+           bird.body.x3, bird.body.y3);
+  pop();
+
+  // Eye
+  push();
+  noStroke();
+  fill(0, 0, 255)
+  ellipse(bird.eye.x, bird.eye.y, bird.eye.size);
+  pop();
+
+  // Move the bird from left to right at a constant velocity
+  bird.body.x1 += bird.velocity.x;
+  bird.body.x2 += bird.velocity.x;
+  bird.body.x3 += bird.velocity.x;
+  bird.eye.x += bird.velocity.x;
 }
