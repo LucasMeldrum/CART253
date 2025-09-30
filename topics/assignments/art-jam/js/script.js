4c20ec670cf3435620845c948797f069a6a5fd9f
LucasMeldrum <150073579+LucasMeldrum@users.noreply.github.com>
2025-09-30 09:42:17 -0400

    added eyes that rotate around the sphere

--- topics/assignments/art-jam/js/script.js
+++ topics/assignments/art-jam/js/script.js
@@ -15,36 +15,71 @@ function setup() {
 }
 
 function draw() {
-  background(95, 50, 115);
+  background(0);
 
-  orbitControl(); // Allow mouse rotation
+  // Allows for control of the canvas, in the 3D space
+  orbitControl();
 
-  // Draw main face-like sphere using little spheres
+  // Draw main face sphere using little spheres
   push();
-  drawSphere(0, 0, 0, 200, 15); 
+  drawSphere(0, 0, 0, 200, 12); 
   pop();
+
+  // Draw facial features (eyes, mouth, etc.)
+  drawFace(0, 0, 0, 200);
 }
 
-function drawSphere(x, y, z, radius, detail) {
-  translate(x, y, z);
-
+function drawSphere(x, y, z, radius, amount) {
   // Loop through latitude and longitude
-  for (let lat = -90; lat <= 90; lat += detail) {
-    for (let lon = 0; lon < 360; lon += detail) {
+  // Calculations done on paper this was annoying, double for loop to iterate through
+  for (let lat = -90; lat <= 90; lat += amount) {
+    for (let lon = 0; lon < 360; lon += amount) {
       let px = radius * cos(lat) * cos(lon);
       let py = radius * sin(lat);
       let pz = radius * cos(lat) * sin(lon);
 
       push();
       translate(px, py, pz);
-      stroke(241,194,125);
-      sphere(7.5); // small sphere size
+      fill(241, 194, 125); 
+      sphere(7.5);
       pop();
     }
   }
 }
 
-  function rotateWithFrameCount() {
+function drawFace(x, y, z, radius) {
+
+  // Rotate the eyes around the face
+  rotateWithFrameCount();
+
+  // Eyes
+  // Left eye
+  push();
+  translate(-60, -50, radius - 10);
+  fill(255);
+  sphere(25);
+
+  // Pupil
+  translate(0, 0, 20);
+  fill(0);
+  sphere(10);
+  pop();
+
+  // Right eye
+  push();
+  translate(60, -50, radius - 10); 
+  fill(255); 
+  sphere(25);
+
+  //Pupil
+  translate(0, 0, 20);
+  fill(0);
+  sphere(10);
+  pop();
+}
+
+// Rotate function found on the p5 documentation
+function rotateWithFrameCount() {
   rotateZ(frameCount);
   rotateX(frameCount);
   rotateY(frameCount);
